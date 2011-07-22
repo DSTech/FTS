b0732d95b2d9eb0bb792a0aeb3932e04e2b51d1a
DSX <MegaDeath409@GMail.com>
2011-07-22 01:36:02 -0400

    Hooked Kills and Money in the Hud.

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,12 +1,13 @@
 kills = 3
 money = 4001
 
+LocalPlayer().HookPNWVar("money", function(mny) money = mny end))
+LocalPlayer().HookPNWVar("kills", function(kls) kills = kls end))
+
 function DrawHud()
-    height = 125
-    width = 350
-    draw.RoundedBoxEx(16,0,ScrH()-height,width,height,Color(0,50,255,200),false,true,false,false)
-    
-    
+	height = 125
+	width = 350
+	draw.RoundedBoxEx(16,0,ScrH()-height,width,height,Color(0,50,255,200),false,true,false,false)
 end
 
 hook.Add("HUDPaint","DrawFTSHud",DrawHud)
