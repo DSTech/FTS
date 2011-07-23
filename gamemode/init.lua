29599ccbc81ca5774903edcd4c7e3c715710f02b
DSX <MegaDeath409@GMail.com>
2011-07-22 21:52:50 -0400

    Added a download reference to cl_hud.lua, to ensure the client received the file upon connection.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,6 +1,7 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 AddCSLuaFile("cl_pnwvar.lua");
+AddCSLuaFile("cl_hud.lua");
 include("sv_pnwvars.lua")
 include("shared.lua")
 include("DataMgmt.lua")
